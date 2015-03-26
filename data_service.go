63cc275bcae66a7d41905cc6ab89848b78b8edee
Charles Korn <ckorn@thoughtworks.com>
2015-03-26 13:58:32 +1100

    Added command line option to control data source URL.

--- data_service.go
+++ data_service.go
@@ -6,11 +6,14 @@ import (
 	"net/http"
 	"time"
 
+	"flag"
 	"github.com/julienschmidt/httprouter"
 	"github.com/stretchr/graceful"
+	"os"
 )
 
 var server *graceful.Server
+var dataSourceName string
 
 func startServer() {
 	router := httprouter.New()
@@ -32,21 +35,30 @@ func stopServer() {
 	server.Stop(10 * time.Second)
 }
 
+func readOptions() {
+	flagSet := flag.NewFlagSet("weather-thingy-data-service", flag.ExitOnError)
+	flagSet.StringVar(&dataSourceName, "dataSource", "postgres://weatherthingy@localhost/weatherthingy?sslmode=disable", "The data source URL to use.")
+	flagSet.Parse(os.Args[1:])
+}
+
 func main() {
+	readOptions()
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	log.Println("Starting up...")
 
 	log.Println("Connecting to database...")
-	db, err := connectToDatabase("postgres://user:pass@localhost/weatherthingy?sslmode=disable")
+	log.Println(dataSourceName)
+	db, err := connectToDatabase(dataSourceName)
 
 	if err != nil {
-		log.Fatal("Could not connect to database.", err)
+		log.Fatal("Could not connect to database: ", err)
 	}
 
 	log.Println("Checking for pending migrations...")
 
 	if n, err := db.runMigrations(); err != nil {
-		log.Fatal("Could not apply migrations to database.", err)
+		log.Fatal("Could not apply migrations to database: ", err)
 	} else {
 		log.Printf("Applied %d migrations.", n)
 	}

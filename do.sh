50c2b35f3ddf1a4c6f7188ed2db5599afa24a201
Charles Korn <ckorn@thoughtworks.com>
2015-08-15 15:31:49 +1000

    Fix syntax.

--- do.sh
+++ do.sh
@@ -119,7 +119,7 @@ function snap-setup {
   sudo yum install --assumeyes mercurial
 
   echo "Installing Docker..."
-  hash docker 2>/dev/null || { curl -sSL https://get.docker.com/ | sh }
+  hash docker 2>/dev/null || { curl -sSL https://get.docker.com/ | sh; }
 
   echo "Environment summary:"
   go env

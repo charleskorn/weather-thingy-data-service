2b460a9a3736db1793395d6e1b688a13d9907773
Charles Korn <me@charleskorn.com>
2016-01-23 17:56:27 +1100

    Added request identifier to log messages.

--- logging_router.go
+++ logging_router.go
@@ -3,19 +3,24 @@ package main
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/go-martini/martini"
+	"github.com/twinj/uuid"
 	"net/http"
 	"time"
 )
 
 func Log() martini.Handler {
 	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
-		log.WithFields(log.Fields{
-			"method":              req.Method,
-			"url":                 req.URL.String(),
-			"remoteAddress":       req.RemoteAddr,
-			"requestLength":       req.ContentLength,
-		}).Info("Request processing started.")
+		requestId := uuid.NewV4().String()
 
+		logger := log.WithFields(log.Fields{
+			"method":        req.Method,
+			"url":           req.URL.String(),
+			"remoteAddress": req.RemoteAddr,
+			"requestLength": req.ContentLength,
+			"requestId":     requestId,
+		})
+
+		logger.Info("Request processing started.")
 		start := time.Now()
 
 		rw := res.(martini.ResponseWriter)
@@ -23,11 +28,7 @@ func Log() martini.Handler {
 
 		elapsed := time.Since(start).Seconds() * 1000
 
-		log.WithFields(log.Fields{
-			"method":              req.Method,
-			"url":                 req.URL.String(),
-			"remoteAddress":       req.RemoteAddr,
-			"requestLength":       req.ContentLength,
+		logger.WithFields(log.Fields{
 			"responseStatus":      rw.Status(),
 			"responseSize":        rw.Size(),
 			"millisecondsElapsed": elapsed,
